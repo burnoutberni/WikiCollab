90a70bb41bcc48cc28ca08072eaf931df8c3d3ee
Bernhard Hayden <hello@bhayden.at>
2026-06-16 10:41:42 +0200

    fix: update shared path alias and remove leftover prompts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      shared: path.resolve(__dirname, '../../shared/src/index.ts'),
+      shared: path.resolve(__dirname, '../shared/src/index.ts'),
     },
   },
   test: {
